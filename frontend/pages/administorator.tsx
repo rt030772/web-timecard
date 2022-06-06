2da9949b1fe58cea56a69057c1ff48e351280039
rt030772 <rt030772@gmail.com>
2022-06-06 22:03:04 +0900

    管理者画面のメニューDrawerを追加

--- frontend/pages/administorator.tsx
+++ frontend/pages/administorator.tsx
@@ -1,10 +1,12 @@
 import { Box, Button, Container, FormControl, FormLabel, Heading, IconButton, Input, ListItem, MenuButton, Select, UnorderedList, VStack } from '@chakra-ui/react'
 import React from 'react';
+import { AdminMenuDrawer } from '../components/layouts/AdminMenuDrawer';
 
 function Administrator() {
   return (
     <>
       <Container>
+        <AdminMenuDrawer/>
         <UnorderedList>
           <ListItem>ユーザー登録(社員番号、名前、email、管理者権限、承認者権限)</ListItem>
           <ListItem>お知らせ</ListItem>
