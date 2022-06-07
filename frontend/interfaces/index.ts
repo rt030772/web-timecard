29f90225a19d5df30f5b269308f6492008410143
r_takahashi <r_takahashi@vision-c@co.jp>
2022-06-07 12:37:47 +0900

    backendのSpringの構造を作成

--- frontend/interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-export interface UserInfo {
-  employeeCode?: string;
-  name?: string;
-  email?: string;
-  password?: string;
-  departmentCode?: number;
-  isAdmin: string;
-  isApprover: string;
-}
